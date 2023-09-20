4e3e72f2ff2d383000f39efac99e1b9597093f07
Camille Onoda <info@camilleonoda.com>
2023-09-20 17:21:31 +0900

    Learn about debugging your code and type of errors

---
+++ Day_1/debugging.py
@@ -0,0 +1,17 @@
+# Fix the code below 👇
+
+"""# SyntaxError : unterminated string literal (detected at line 3)
+print(Day 1 - String Manipulation")
+# Concatenating both strings while we want the '+' sign to be part of the string printed.
+print("String Concatenation is done with the "+" sign.")
+# IndentationError: unexpected indent
+  print('e.g. print("Hello " + "world")')
+# SyntaxError: '(' was never closed
+print(("New lines can be created with a backslash and n.")"""
+
+
+# Correct code
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the" ' "+" ' "sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
