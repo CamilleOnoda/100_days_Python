9edad229967ac110f1cbdb2ac29549c817278bdc
Camille Onoda <info@camilleonoda.com>
2023-09-28 17:36:59 +0900

    Make a Hangman word guessing game

---
+++ Day_7/hangman.py
@@ -0,0 +1,108 @@
+import random
+
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+words = ["rubicon", "starfield", "harry", "hokkaido"]
+
+# Generate a random word from the list
+rand_word = random.choice(words)
+word_length = len(rand_word)
+
+
+# Generate as many blanks as letters in word
+display = []
+for _ in range(word_length):
+    display += "_"
+
+
+# Initiate the game
+game_over = False
+life = 6
+while not game_over:
+    guess = input("\nChoose a letter: ").lower()
+
+    # If letter in word = replace the blank with the letter
+    for position in range(word_length):
+        letter = rand_word[position]
+        if letter == guess:
+            display[position] = guess
+
+
+    # If letter not in word = lose a life
+    # If the user run out of life = Game Over. Game stops
+    if guess not in rand_word:
+        life -= 1
+        if life == 0:
+            game_over = True
+            print("Game Over!")
+            print(f"The word was {rand_word}")
+
+
+    # If all the blanks are filled, the user wins and the game stops
+    if "_" not in display:
+        game_over = True
+        print("You won!")
+
+    
+    # Convert the list to a string
+    print(f"{''.join(display)}")
+
+
+    # Print the hangman image corresponding to the current number of lives
+    print(stages[life])
