6db8a6df2ae6b0c383073783e2556caf786ea101
Camille Onoda <info@camilleonoda.com>
2023-09-30 13:35:57 +0900

    Use functions, keyword and positional arguments

---
+++ Day_8/greet.py
@@ -0,0 +1,17 @@
+"""def greet():
+  name = input("Hi! What's your name?\n")
+  print(f"Hello {name.title()}!")
+  print("What a nice weather today!")
+  print("Hope you're having a great day :)\n")
+
+
+greet()"""
+
+# Paramater = name
+def greet_with(name, location):
+    print(f"Hello {name.capitalize()}")
+    print(f"How do you do {name.capitalize()}?")
+    print(f"How is it like in {location.capitalize()}?")
+
+# Argument = Camille
+greet_with(location='nagoya', name="Camille")
