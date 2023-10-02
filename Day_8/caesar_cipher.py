fe654fd01aef68375226567bc0a4b9cafc64ca83
Camille Onoda <info@camilleonoda.com>
2023-10-02 13:46:41 +0900

    Encode or decode a message

---
+++ Day_8/caesar_cipher.py
@@ -0,0 +1,35 @@
+from art import logo
+print(logo)
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(text, shift, direction):
+    end_text = ""
+    if direction == "decode":
+            shift *= -1
+    for letter in text:
+        position = alphabet.index(letter)
+
+        new_position = position + shift
+        end_text += alphabet[new_position]
+    
+    print(f"The {direction}d text is {end_text}")
+
+
+end_session = False
+while not end_session:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to try again. Otherwise type 'no'.\n")
+    if restart == 'no':
+         end_session = True
+         print("See you next time!")
