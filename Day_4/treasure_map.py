b4d046756fe808f5b6dd2eed4bf5bb79236c22d4
Camille Onoda <info@camilleonoda.com>
2023-10-02 14:06:40 +0900

    Write a program that will mark a spot with an X

--- Day_4/treasure_map.py
+++ Day_4/treasure_map.py
@@ -2,15 +2,17 @@ row1 = ["⬜️","️⬜️","️⬜️"]
 row2 = ["⬜️","⬜️","️⬜️"]
 row3 = ["⬜️️","⬜️️","⬜️️"]
 map = [row1, row2, row3]
-
-print(f"{row1}\n{row2}\n{row3}")
+print(map)
+#print(f"{row1}\n{row2}\n{row3}")
 
 position = input("Where do you want to put the treasure? ")
 
-row = int(position[0])
-column = int(position[1])
+column = int(position[0])
+row = int(position[1])
 
-select_row = map[column-1]
-select_row[row -1] = "X"
+#select_row_column = map[row-1]
+#select_row_column[column -1] = "X"
+
+map[row - 1][column - 1] = 'X'
 
 print(f"{row1}\n{row2}\n{row3}")
