045cb7b56f4570fad4556aa04f1a3d18e0e11aa7
Camille Onoda <info@camilleonoda.com>
2023-10-06 10:51:11 +0900

    Update the find_max_bidder function to allow users to bid again in case of a tie

--- Day_9/secret_auction.py
+++ Day_9/secret_auction.py
@@ -1,7 +1,11 @@
 from art import logo
 import os
+import platform
+
+clear_command = 'cls' if platform.system() == 'Windows' else 'clear'
 
 print(logo)
+print("Welcome to the Silent Auction")
 
 bids = {}
 bid_history = {}
@@ -29,20 +33,33 @@ def new_bidder():
 
 def find_max_bidder():
     if bids:
-        # Telling Python to find the key (i.e., bidder's name) in the bids dictionary 
-        # that corresponds to the maximum value (i.e., highest bid price).
-        # .get() is used to retrieve the value associated with a given key in a dict.
-        max_bidder = max(bids, key=bids.get)
-        max_price = bids[max_bidder]
-        print(f"The winner is {max_bidder} with ${max_price}.")
+        max_bid_price = max(bids.values())
+        # Create a list of bidder names for the bidders whose bid price matches the maximum bid price
+        max_bidders = [name for name, price in bids.items() if price == max_bid_price]
+
+        if len(max_bidders) == 1:
+            max_bidder = max_bidders[0]
+            max_price = max_bid_price
+            print(f"The winner is {max_bidder} with ${max_price}.")         
+        else:
+            print("It's a tie! The following bidders have the highest bid:")
+            for bidder in max_bidders:
+                print(f"{bidder} with ${max_bid_price}")
+            
+            # Clear bids and allow bidders to bid again
+            bids.clear()
+            new_bidders = input("Would you like to continue with new bids? ").lower()
+            if new_bidders == "yes":
+                clear_console()
+                main()
+            else:
+                print("Auction ended.")         
     else:
         print("No bids were entered.")
 
     
-# To clear the console
 def clear_console():
-    os.system('cls')
-
+    os.system(clear_command)
 
 if __name__ == "__main__":
     main()
