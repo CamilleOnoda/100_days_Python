3934b738f6e2ba86e587f88d6aacbee47ff80d3c
Camille Onoda <info@camilleonoda.com>
2023-10-11 11:54:45 +0900

    Refactor code

--- Day_12/number_guessing_game.py
+++ Day_12/number_guessing_game.py
@@ -5,8 +5,6 @@ import time
 print(logo)
 
 def main():
-    global level
-
     print("Let's begin...")
     print("I'm going to pick a number between 1 and 100.")
     time.sleep(2)
@@ -18,54 +16,37 @@ def main():
         print("You must type 'easy' or 'hard'.")
         level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
-    play_game()
+    play_game(level)
 
 
-def play_game():
+def play_game(level):
     correct_number = randint(1, 100)
     max_attemps = {'easy': 10, 'hard': 5}
     attempts = max_attemps[level]
-    easy_level = 10
-    hard_level = 5
     player_guess = []
-    player_attempt = 0
 
-    if level == 'easy':
-        print(f"You have {easy_level} attempts remaining to guess the number.")
-    elif level == 'hard':
-        print(f"You have {hard_level} attempts remaining to guess the number.")
-        
-    while player_guess != correct_number:
+
+    while attempts > 0:
+        print(f"You have {attempts} attempts remaining to guess the number.")
         guess = int(input("Make a guess: "))
         if guess in player_guess:
             print("You've already used this number! Choose another one.")
-        elif guess == correct_number:
+            continue
+
+        player_guess.append(guess)
+        attempts -= 1
+
+        if guess == correct_number:
             print(f"You win! You guessed the correct number: {correct_number}.")
             break
-        else:
-            player_guess.append(guess)
-            player_attempt += 1
-
-            if level == 'easy':
-                attempts = easy_level - player_attempt
-                if attempts == 0:
-                    print(f"You lose. The correct number was: {correct_number}.")
-                    break
-                else:
-                    print(f"You have {attempts} attempts remaining to guess the number.")
-
-            elif level == 'hard':
-                attempts = hard_level - player_attempt
-                if attempts == 0:
-                    print(f"You lose. The correct number was: {correct_number}.")
-                    break
-                else:
-                    print(f"You have {attempts} attempts remaining to guess the number.")
-
-        if guess > correct_number:
+        elif guess > correct_number:
             print("Too high!")
-        elif guess < correct_number:
+        else:
             print("Too low!")
+
+        if attempts == 0:
+            print(f"You lose! The correct number was: {correct_number}!")
+            break
 
 
 if __name__ == "__main__":
