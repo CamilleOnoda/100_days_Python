e234b588f10fa4f749a7e86c1d2f67d281eece8b
Camille Onoda <info@camilleonoda.com>
2023-10-14 07:14:20 +0900

    Menu and resources for the coffee machine

---
+++ Day_15/machine_requirements.py
@@ -0,0 +1,32 @@
+MENU = {
+    'espresso': {
+        'ingredients': {
+            'water': 50,
+            'coffee': 18,
+        },
+        'cost': 1.5,
+    },
+    'latte': {
+        'ingredients': {
+            'water': 200,
+            'coffee':150,
+            'milk': 24,
+        },
+    },
+    'cappuccino': {
+        'ingredients': {
+            'water': 250,
+            'milk':100,
+            'coffee': 24,
+        },
+    },
+}
+
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0,
+}
+
