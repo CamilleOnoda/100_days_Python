9194930aca17631fad8a8e29f0b27eb4b7dc0988
Camille Onoda <info@camilleonoda.com>
2023-10-14 09:15:24 +0900

    Flag to turn off the machine

--- Day_15/coffee_machine.py
+++ Day_15/coffee_machine.py
@@ -3,16 +3,20 @@ from machine_requirements import resources, MENU
 
 def main():
     turn_off = False
-    
+
     while not turn_off:
-        order = input("What would you like? (espresso/latte/cappuccino): " )
-        for resource, amount in resources.items():
-            if resource in ["water", "milk"]:
-                print(f"{resource.capitalize()}: {amount}ml")
-            elif resource == "coffee":
-                print(f"{resource.capitalize()}: {amount}g")
-            elif resource == "money":
-                print(f"{resource}: {amount}$")
+        order = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+        if order == 'off':
+            turn_off = True
+        if order == 'report':
+            for resource, amount in resources.items():
+                if resource in ["water", "milk"]:
+                    print(f"{resource.capitalize()}: {amount}ml")
+                elif resource == "coffee":
+                    print(f"{resource.capitalize()}: {amount}g")
+                elif resource == "money":
+                    print(f"{resource.capitalize()}: {amount}$")
             
 
 
