455b82398293804e952102a3225d54c59a2aed25
Camille Onoda <info@camilleonoda.com>
2023-10-14 09:31:51 +0900

    Start function to check if resources are sufficient

--- Day_15/coffee_machine.py
+++ Day_15/coffee_machine.py
@@ -9,6 +9,7 @@ def main():
 
         if order == 'off':
             turn_off = True
+
         if order == 'report':
             for resource, amount in resources.items():
                 if resource in ["water", "milk"]:
@@ -18,7 +19,15 @@ def main():
                 elif resource == "money":
                     print(f"{resource.capitalize()}: {amount}$")
             
-
+        if order == "espresso":
+            espresso_amount_water = MENU["espresso"]["ingredients"]["water"]
+            espresso_amount_coffee = MENU["espresso"]["ingredients"]["coffee"]
+            
+            if espresso_amount_water > resources["water"]:
+                print("Sorry, there is not enough water.")
+            elif espresso_amount_coffee > resources["coffee"]:
+                print("Sorry, there is not enough coffee.")
+            
 
 
 
