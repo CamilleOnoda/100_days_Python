00c23d7ac95d854fd3eddf36e624169a37dd4da9
Camille Onoda <info@camilleonoda.com>
2023-10-22 07:38:56 +0900

    Decode HTML entities before displaying the questions

--- Day_17/quiz_project.py
+++ Day_17/quiz_project.py
@@ -1,10 +1,11 @@
 from question_model import Question
 from trivia_data import question_data
 from quiz_brain import QuizBrain
+import html
 
 question_bank = []
 for question_dict in question_data:
-    question_text = question_dict["question"]
+    question_text = html.unescape(question_dict["question"])
     question_answer = question_dict["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
