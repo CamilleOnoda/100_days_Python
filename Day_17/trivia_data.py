44721c1c091d48021e8f63144ade36d7c3d8d145
Camille Onoda <info@camilleonoda.com>
2023-10-22 07:35:25 +0900

    Quiz game

---
+++ Day_17/trivia_data.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import html
+
+
+response_API = requests.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean")
+data = json.loads(response_API.text)
+
+question_data = data["results"]
+
