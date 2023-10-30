24a098050ed324a1114b3677617503f31df073bd
Camille Onoda <info@camilleonoda.com>
2023-10-30 15:08:45 +0900

    Generate a random password based on the length defined by the user. The user can copy the password to clipboard.

--- Day_29/main.py
+++ Day_29/main.py
@@ -1,31 +1,11 @@
-from cgitb import text
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
+import string
 
 
-# Save data in a file
-def save():
-    global add_button
-
-    website = website_entry.get()
-    email = email_username_entry.get()
-    password = password_entry.get()
-
-    if not website or not email or not password:
-        error_label.config(text="Please fill in all required information", fg="red")
-    else:
-        validate = messagebox.askokcancel(title=website, message=f"Emai: {email} \n Password: {password} \n Do you want to save?")
-        if validate:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-
-        messagebox.showinfo(title=None, message="Data saved!")
-        error_label.config(text="")
-
-
-# User interface
+# -----------------------User interface-----------------------------------------
 window = Tk()
 window.title("Password Manager")
 window.config(padx=30, pady=30)
@@ -36,35 +16,84 @@ canvas.create_image(130, 100, image=img)
 canvas.grid(column=1, row=0)
 
 
-# Labels
+# -----------------------Save data in a file------------------------------------
+def save():
+    global add_button
+
+    website = website_entry.get()
+    email = email_username_entry.get()
+    password = password_entry.get()
+
+    if not website or not email or not password:
+        error_label.config(text="Please fill in all required information", fg="red")
+    else:
+        validate = messagebox.askokcancel(title=website, message=f"Website: {website} \n"
+                                          f" Emai: {email} \n Password: {password} \n Do you want to save?")
+        if validate:
+            with open("data.txt", "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+            messagebox.showinfo(title=None, message="Data saved!")
+            error_label.config(text="")
+
+
+# -----------------------Generate random password-------------------------------
+# -----------------------Get the length of the password-------------------------
+password_len = IntVar()
+length = Spinbox(from_= 6, to_ = 15, textvariable=password_len)
+length.grid(column=2, row=4)
+
+generated_password = StringVar()
+combination = [string.punctuation, string.ascii_uppercase, string.digits, string.ascii_lowercase]
+def randPassGen():
+    password = ""
+    for char in range(password_len.get()):
+        char = random.choice(combination)
+        password = password + random.choice(char)
+    generated_password.set(password)
+
+
+# -----------------------Copy to clipboard--------------------------------------
+def copy_password():
+    pyperclip.copy(generated_password.get())
+    messagebox.showinfo(title=None, message="Password copied to clipboard.")
+
+
+# -----------------------Labels-------------------------------------------------
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 email_username_label = Label(text="Email/Username:")
 email_username_label.grid(column=0, row=2)
-password_label = Label(text="Password:")
+password_label = Label(text="Random Password:")
 password_label.grid(column=0, row=3)
+password_length_label = Label(text="Password length (6-15)")
+password_length_label.grid(column=2, row=3)
 
 
-# Entries
+# -----------------------Entries------------------------------------------------
 website_entry = Entry(width=50)
 website_entry.grid(column=1,row=1, columnspan=2)
 website_entry.focus()
 email_username_entry = Entry(width=50)
 email_username_entry.grid(column=1, row=2, columnspan=2)
 email_username_entry.insert(0, "info@camilleonoda.com")
-password_entry = Entry(width=30)
+password_entry = Entry(textvariable=generated_password, width=28)
 password_entry.grid(column=1, row=3)
 
 
-# Buttons
-generate_password_button = Button(text="Generate Password")
-generate_password_button.grid(column=2, row=3, padx=(0,10))
+# -----------------------Buttons------------------------------------------------
+generate_password_button = Button(text="Generate Password", command=randPassGen)
+generate_password_button.grid(column=2, row=5, padx=(0,10))
 add_button = Button(text="Add", command=save)
-add_button.grid(column=2, row=4, ipadx=5, padx=(0,80))
+add_button.grid(column=2, row=6, ipadx=5, padx=(0,80))
+clipboard = Button(text="Copy to clipboard", command=copy_password)
+clipboard.grid(column=1, row=4, padx=(0,62))
 
 
-# Error label
+# -----------------------Error label--------------------------------------------
 error_label = Label(text="", fg="red")
 error_label.grid(column=1, row=5)
+
 
 window.mainloop()
