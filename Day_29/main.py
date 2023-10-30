30028c2cac209d787917effe8f679a0a214a4c38
Camille Onoda <info@camilleonoda.com>
2023-10-30 13:50:37 +0900

    Take input from the user and save data to a file

--- Day_29/main.py
+++ Day_29/main.py
@@ -1,7 +1,30 @@
+from cgitb import text
 from tkinter import *
+from tkinter import messagebox
 
 
+# Save data in a file
+def save():
+    global add_button
 
+    website = website_entry.get()
+    email = email_username_entry.get()
+    password = password_entry.get()
+
+    validate = messagebox.askokcancel(title=website, message=f"Emai: {email} \n Password: {password} \n Do you want to save?")
+
+    if not website or not email or not password:
+        error_label.config(text="Please fill in all required information", fg="red")
+    elif validate:
+        with open("data.txt", "a") as file:
+            file.write(f"{website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+        messagebox.showinfo(title=None, message="Data saved!")
+
+
+# User interface
 window = Tk()
 window.title("Password Manager")
 window.config(padx=30, pady=30)
@@ -12,6 +35,7 @@ canvas.create_image(130, 100, image=img)
 canvas.grid(column=1, row=0)
 
 
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 email_username_label = Label(text="Email/Username:")
@@ -20,22 +44,26 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 
+# Entries
 website_entry = Entry(width=50)
 website_entry.grid(column=1,row=1, columnspan=2)
+website_entry.focus()
 email_username_entry = Entry(width=50)
 email_username_entry.grid(column=1, row=2, columnspan=2)
+email_username_entry.insert(0, "info@camilleonoda.com")
 password_entry = Entry(width=30)
 password_entry.grid(column=1, row=3)
 
 
+# Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3, padx=(0,10))
-add_button = Button(text="Add")
+add_button = Button(text="Add", command=save)
 add_button.grid(column=2, row=4, ipadx=5, padx=(0,80))
 
 
-
-
-
+# Error label
+error_label = Label(text="", fg="red")
+error_label.grid(column=1, row=5)
 
 window.mainloop()
