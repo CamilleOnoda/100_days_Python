5879258779052dd16f94517a2f49fb9933401ae1
Camille Onoda <info@camilleonoda.com>
2023-10-31 10:14:45 +0900

    Built with object-oriented programming. Properly handles the User / PasswordManager / PasswordPrompt class

--- Day_29/main.py
+++ Day_29/main.py
@@ -7,227 +7,214 @@ import string
 import os
 
 
-password_prompt_entry = None
-password_prompt_label = None
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.password_manager = PasswordManager()
 
-# -----------------------Password prompt interface------------------------------
-password_prompt = Tk()
-password_prompt.title("Password Prompt")
-password_prompt.config(padx=30, pady=30)
-#password_prompt_canvas = Canvas(password_prompt, width=300, height=100)
-#password_prompt_canvas.create_image(200, 200)
+class PasswordManager:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Password Manager")
+        self.window.config(padx=30, pady=30)
+        self.canvas = Canvas(width=200, height=200)
+        img = PhotoImage(file="logo.png")
+        self.window.iconphoto(False, img)
+        self.canvas.create_image(130, 100, image=img)
+        self.canvas.image = img
+        self.canvas.grid(column=1, row=0)
 
+        # Initialize encryption key
+        self.encryption_key = self.load_key()
 
-# -----------------------Check Password-----------------------------------------
-def check_password():
-    global password_prompt_entry, password_prompt_label
-    entered_password = password_prompt_entry.get()
-    if entered_password == "1234":
-        password_prompt.destroy()
-        main_program()
-    else:
-        password_prompt_entry.delete(0, 'end')
-        password_prompt_label.config(text="Incorrect password", fg="red")
+        # Create and configure UI elements
+        self.create_ui()
 
+    def create_ui(self):
+        self.website_label = Label(text="Website:")
+        self.website_label.grid(column=0, row=1)
+        self.email_username_label = Label(text="Email/Username:")
+        self.email_username_label.grid(column=0, row=2)
+        self.password_label = Label(text="Random Password:")
+        self.password_label.grid(column=0, row=3)
+        self.password_len = IntVar()
+        self.password_length_label = Label(text="Password length (6-15)")
+        self.password_length_label.grid(column=2, row=3)
+        self.length = Spinbox(from_=6, to_=15, textvariable=self.password_len)
+        self.length.grid(column=2, row=4)
 
-password_prompt_label = Label(password_prompt, text="Enter password:")
-password_prompt_label.grid(column=0, row=1)
+        self.website_entry = Entry(width=50)
+        self.website_entry.grid(column=1, row=1, columnspan=2)
+        self.website_entry.focus()
+        self.email_username_entry = Entry(width=50)
+        self.email_username_entry.grid(column=1, row=2, columnspan=2)
+        self.email_username_entry.insert(0, "info@camilleonoda.com")
+        self.password_entry = Entry(width=28)
+        self.password_entry.grid(column=1, row=3)
 
-password_prompt_entry = Entry(password_prompt, show="*")
-password_prompt_entry.grid(column=1, row=1)
+        self.generated_password = StringVar()
+        self.generate_password_button = Button(text="Generate Password", command=self.rand_pass_gen)
+        self.generate_password_button.grid(column=2, row=5, padx=(0, 10))
+        self.add_button = Button(text="Add", command=self.save)
+        self.add_button.grid(column=2, row=6, ipadx=5, padx=(0, 79))
+        self.clipboard = Button(text="Copy to clipboard", command=self.copy_password)
+        self.clipboard.grid(column=1, row=4, padx=(0, 62))
+        self.search_button = Button(text="Search Password", command=self.search_password)
+        self.search_button.grid(column=2, row=7, padx=(0, 24))
 
-password_submit_password = Button(password_prompt, text="Submit", command=check_password)
-password_submit_password.grid(column=1,row=1)
+        self.error_label = Label(text="", fg="red")
+        self.error_label.grid(column=1, row=5)
 
-# -----------------------User interface-----------------------------------------
-def main_program():
-    window = Tk()
-    window.title("Password Manager")
-    window.config(padx=30, pady=30)
-    canvas = Canvas(width=200,height=200)
-    img = PhotoImage(file="logo.png")
-    window.iconphoto(False, img)
-    canvas.create_image(130, 100, image=img)
-    canvas.grid(column=1, row=0)
+    def load_key(self):
+        key_file_path = "secret.key"
+        if not os.path.isfile(key_file_path):
+            self.generate_key()
+        with open(key_file_path, "rb") as key_file:
+            return key_file.read()
 
-
-# -----------------------Encryption key handling--------------------------------
-    def generate_key():
+    def generate_key(self):
         key = Fernet.generate_key()
         with open("secret.key", "wb") as key_file:
             key_file.write(key)
 
+    def encrypt_data(self, data):
+        fernet = Fernet(self.encryption_key)
+        encrypted_data = fernet.encrypt(data.encode())
+        return encrypted_data
 
-    def load_key():
-        key_file_path = "secret.key"
-        if not os.path.isfile(key_file_path):
-            generate_key()
-        with open(key_file_path, "rb") as key_file:
-            return key_file.read()
+    def decrypt_data(self, encrypted_data):
+        fernet = Fernet(self.encryption_key)
+        decrypted_data = fernet.decrypt(encrypted_data).decode()
+        return decrypted_data
 
-
-    encryption_key = load_key()
-
-
-    # -----------------------Data processing------------------------------------
-    def save():
-        """Save data into a txt file."""
-        website = website_entry.get().lower()
-        email = email_username_entry.get()
-        password = password_entry.get()
+    def save(self):
+        website = self.website_entry.get().lower()
+        email = self.email_username_entry.get()
+        password = self.password_entry.get()
 
         if not website or not email or not password:
-            error_label.config(text="Please fill in all required information", fg="red")
+            self.error_label.config(text="Please fill in all required information", fg="red")
         else:
-            encrypted_password = encrypt_data(password, encryption_key)
+            encrypted_password = self.encrypt_data(password)
+            lines = self.read_data()
 
-            lines = read_data()
-
-            if website_exists(website, lines):
-                update_data(website, email, encrypted_password, lines)
+            if self.website_exists(website, lines):
+                self.update_data(website, email, encrypted_password, lines)
             else:
-                validate = messagebox.askokcancel(title=website, message=f"Website: {website} \n"
-                                            f" Emai: {email} \n Password: {password} \n Do you want to save?")
+                validate = messagebox.askokcancel(
+                    title=website, message=f"Website: {website} \n Emai: {email} \n Password: {password} \n Do you want to save?"
+                )
                 if validate:
                     with open("data.txt", "a") as file:
                         file.write(f"{website} | {email} | {encrypted_password}\n")
-                    clear_entries()
+                    self.clear_entries()
                     messagebox.showinfo(title=None, message="Data saved!")
-                    error_label.config(text="")
+                    self.error_label.config(text="")
 
+    def clear_entries(self):
+        self.website_entry.delete(0, END)
+        self.password_entry.delete(0, END)
 
-    def clear_entries():
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
-
-
-    def read_data():
-        """Read the txt file."""
+    def read_data(self):
         with open("data.txt", "r") as file:
             lines = file.readlines()
         return lines
 
-
-    def website_exists(website, lines):
-        """Verify if the data entered already exists."""
+    def website_exists(self, website, lines):
         for x, line in enumerate(lines):
             data = line.strip().split(" | ")
             if data[0].lower() == website:
                 return x
-        return None        
+        return None
 
-
-    def update_data(website, email, password, lines):
-        """If the data exists, ask the user if they want to update the information."""
-        update = messagebox.askyesno(title=website, message=f"The website '{website}'"
-                                                " already exists in the data file."
-                                                " Do you want to update its information?")
+    def update_data(self, website, email, password, lines):
+        update = messagebox.askyesno(
+            title=website,
+            message=f"The website '{website}' already exists in the data file. Do you want to update its information?",
+        )
         if update:
-            index = website_exists(website, lines)
+            index = self.website_exists(website, lines)
             if index is not None:
                 lines[index] = f"{website} | {email} | {password}\n"
                 with open("data.txt", "w") as file:
                     file.writelines(lines)
-                clear_entries()
+                self.clear_entries()
                 messagebox.showinfo(title=None, message="Data updated!")
 
-
-    # -----------------------Generate random password---------------------------
-    # -----------------------Get the length of the password---------------------
-    password_len = IntVar()
-    length = Spinbox(from_= 6, to_ = 15, textvariable=password_len)
-    length.grid(column=2, row=4)
-
-    generated_password = StringVar()
-    combination = [string.punctuation, string.ascii_uppercase, string.digits, string.ascii_lowercase]
-    def randPassGen():
+    def rand_pass_gen(self):
         password = ""
-        for char in range(password_len.get()):
-            char = random.choice(combination)
-            password = password + random.choice(char)
-        generated_password.set(password)
+        password_len = int(self.password_len.get())
+        combination = [string.punctuation, string.ascii_uppercase, string.digits, string.ascii_lowercase]
+        character = ''.join(combination)
 
+        for _ in range(password_len):
+            password += random.choice(character)
+        self.password_entry.insert(0, password)
 
-    def get_decrypted_password(website):
-        lines = read_data()
+    def get_decrypted_password(self, website):
+        lines = self.read_data()
         for line in lines:
             data = line.strip().split(" | ")
             if data[0].lower() == website.lower():
-                encrypted_password = data[2][2:-1]  # Extract the encrypted password from the line
-                decrypted_password = decrypt_data(encrypted_password, encryption_key)
+                encrypted_password = data[2][2:-1]
+                decrypted_password = self.decrypt_data(encrypted_password)
                 return decrypted_password
 
-        return None  # Return None if the website is not found
+        return None
 
-    def search_password():
-        website = website_entry.get()
-        decrypted_password = get_decrypted_password(website)
-        
+    def search_password(self):
+        website = self.website_entry.get()
+        decrypted_password = self.get_decrypted_password(website)
+
         if decrypted_password:
             pyperclip.copy(decrypted_password)
             messagebox.showinfo(title=None, message=f"Password for {website} copied to clipboard.")
         else:
             messagebox.showerror(title=None, message=f"Password for {website} not found or could not be decrypted.")
 
-
-    # -----------------------Copy to clipboard----------------------------------
-    def copy_password():
-        pyperclip.copy(generated_password.get())
+    def copy_password(self):
+        pyperclip.copy(self.generated_password.get())
         messagebox.showinfo(title=None, message="Password copied to clipboard.")
 
+    def run(self):
+        self.window.mainloop()
 
-    # -----------------------Data encryption and decryption---------------------
-    def encrypt_data(data, key):
-        fernet = Fernet(key)
-        encrypted_data = fernet.encrypt(data.encode())
-        return encrypted_data
+class PasswordPrompt:
+    def __init__(self):
+        self.password_prompt = Tk()
+        self.password_prompt.title("Enter your credentials")
+        self.password_prompt.config(padx=30, pady=30)
+        self.password_prompt.geometry("300x150")
 
+        self.username_label = Label(self.password_prompt, text="Username:")
+        self.username_label.grid(column=0, row=1)
+        self.username_entry = Entry(self.password_prompt)
+        self.username_entry.grid(column=1, row=1)
+        self.username_entry.focus()
 
-    def decrypt_data(encrypted_data, key):
-        fernet = Fernet(key)
-        decrypted_data = fernet.decrypt(encrypted_data).decode()
-        return decrypted_data
+        self.password_prompt_label = Label(self.password_prompt, text="Password:")
+        self.password_prompt_label.grid(column=0, row=2)
+        self.password_prompt_entry = Entry(self.password_prompt, show="*")
+        self.password_prompt_entry.grid(column=1, row=2)
 
+        self.password_submit_password = Button(self.password_prompt, text="Submit", command=self.check_credentials)
+        self.password_submit_password.grid(column=1, row=3)
 
-    # -----------------------Labels---------------------------------------------
-    website_label = Label(text="Website:")
-    website_label.grid(column=0, row=1)
-    email_username_label = Label(text="Email/Username:")
-    email_username_label.grid(column=0, row=2)
-    password_label = Label(text="Random Password:")
-    password_label.grid(column=0, row=3)
-    password_length_label = Label(text="Password length (6-15)")
-    password_length_label.grid(column=2, row=3)
+    def check_credentials(self):
+        entered_username = self.username_entry.get()
+        entered_password = self.password_prompt_entry.get()
+        if entered_username == "camille" and entered_password == "1234":
+            self.password_prompt.destroy()
+            user = User(entered_username, entered_password)
+            user.password_manager.run()
+        else:
+            self.credentials_error = Label(text="Incorrect username or password", fg="red")
+            self.credentials_error.grid(column=1, row=4)
 
+    def run(self):
+        self.password_prompt.mainloop()
 
-    # -----------------------Entries--------------------------------------------
-    website_entry = Entry(width=50)
-    website_entry.grid(column=1,row=1, columnspan=2)
-    website_entry.focus()
-    email_username_entry = Entry(width=50)
-    email_username_entry.grid(column=1, row=2, columnspan=2)
-    email_username_entry.insert(0, "info@camilleonoda.com")
-    password_entry = Entry(textvariable=generated_password, width=28)
-    password_entry.grid(column=1, row=3)
-
-
-    # -----------------------Buttons--------------------------------------------
-    generate_password_button = Button(text="Generate Password", command=randPassGen)
-    generate_password_button.grid(column=2, row=5, padx=(0,10))
-    add_button = Button(text="Add", command=save)
-    add_button.grid(column=2, row=6, ipadx=5, padx=(0,79))
-    clipboard = Button(text="Copy to clipboard", command=copy_password)
-    clipboard.grid(column=1, row=4, padx=(0,62))
-    search_button = Button(text="Search Password", command=search_password)
-    search_button.grid(column=2, row=7, padx=(0,24))
-
-
-    # -----------------------Error label----------------------------------------
-    error_label = Label(text="", fg="red")
-    error_label.grid(column=1, row=5)
-
-
-    window.mainloop()
-
-
-password_prompt.mainloop()
+if __name__ == "__main__":
+    password_prompt = PasswordPrompt()
+    password_prompt.run()
