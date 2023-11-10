70519369e451ab6799de2f3c13ed656b86409139
Camille Onoda <info@camilleonoda.com>
2023-11-10 15:06:50 +0900

    Change the picture and add a background image

--- Day_33/main.py
+++ Day_33/main.py
@@ -13,15 +13,14 @@ def get_quote():
 
 window = Tk()
 window.title("Zen quotes...")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=600, height=150)
-#background_img = PhotoImage(file="background.png")
-#canvas.create_image(300, 200, image=background_img)
-quote_text = canvas.create_text(300, 90, text="", width=250, font=("Verdana", 12, "bold"), fill="salmon")
+window.config(padx=30, pady=30)
+canvas = Canvas(width=600, height=200)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(300, 200, image=background_img)
+quote_text = canvas.create_text(300, 110, text="", width=200, font=("Verdana", 10, "bold"), fill="black")
 canvas.grid(row=0, column=0)
 
-img = PhotoImage(file="grandpa.png")
+img = PhotoImage(file="grandpa3.png")
 grandpa_button = Button(image=img, highlightthickness=0, command=get_quote)
 grandpa_button.grid(row=1, column=0)
 
