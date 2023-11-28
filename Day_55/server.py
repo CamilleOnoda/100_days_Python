7854d8c6c39c643ffb76b36dc79d82cf17fdb63d
Camille Onoda <info@camilleonoda.com>
2023-11-28 18:17:55 +0900

    Flask app to guess a number. Create a route that detects the number entered by the user and checks it against the random generated number.

--- Day_55/server.py
+++ Day_55/server.py
@@ -1,10 +1,37 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def guess_nb():
+def game():
     return "<div style='text-align: center'>"\
         "<h1 style='text-align: center'>Guess a number between 0 and 9</h1>"\
         "<img style='display: block; margin: auto' src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"\
         "</div>"
+
+@app.route('/<int:number>')
+def guess_nb(number):
+    correct_number = random.randint(0, 9)
+    number_found = False
+    while not number_found:
+        if number == correct_number:
+            number_found = True
+            return "<div style='text-align: center'>"\
+            "<h1 style=text-align: center; color: orange'>You found me!</h1>"\
+            "<img style='display: block; margin: auto' src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXlkZGozZWJ5dWJ1NWRydWE2YW8xamVwZmxkOTc0NzE5OGRuNzN4dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/cXblnKXr2BQOaYnTni/giphy.gif'>"
+        elif number < correct_number:
+            return "<div style='text-align: center'>"\
+            "<h1 style=text-align: center; color: green'>Too low! Try again!</h1>"\
+            "<img style='display: block; margin: auto' src='https://media.giphy.com/media/ljtfkyTD3PIUZaKWRi/giphy.gif'>"
+        elif number > correct_number:
+            return "<div style='text-align: center'>"\
+            "<h1 style=text-align: center; color: blue'>Too high! Try again!</h1>"\
+            "<img style='display: block; margin: auto' src='https://media.giphy.com/media/BY8ORoRpnJDXeBNwxg/giphy.gif'>"
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
