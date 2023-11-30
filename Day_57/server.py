a71c8a0298b0923a6d7e7f033a791ec40efe9b12
Camille Onoda <info@camilleonoda.com>
2023-12-01 05:48:12 +0900

    Create a route to blog posts

--- Day_57/server.py
+++ Day_57/server.py
@@ -16,6 +16,7 @@ def home():
 def guess(name):
     gender_url = f"https://api.genderize.io?name={name}"
     gender_response = requests.get(gender_url)
+    gender_response.raise_for_status()
     gender_data = gender_response.json()
     gender = gender_data["gender"]
     age_url = f"https://api.agify.io?name={name}"
@@ -24,6 +25,13 @@ def guess(name):
     age = age_data["age"]
     return render_template("guess.html", name=name, gender=gender, age=age)
 
+
+@app.route('/blog')
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
