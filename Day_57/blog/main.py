03df093fc8671cbe41344a94836677826f2143b5
Camille Onoda <info@camilleonoda.com>
2023-12-04 16:56:43 +0900

    Set up routes to display the blog's main page and each post

---
+++ Day_57/blog/main.py
@@ -0,0 +1,25 @@
+from ftplib import all_errors
+from flask import Flask, render_template
+import requests
+from post import Post
+
+
+app = Flask(__name__)
+
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+response = requests.get(blog_url)
+all_posts = response.json()
+
+
+@app.route('/')
+def blog():
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/blog/<int:index>')
+def get_post(index):
+    return render_template("post.html", posts=all_posts, id=index)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
