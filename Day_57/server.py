ad76dd37613fc1e24062e922032d325099aa5f0a
Camille Onoda <info@camilleonoda.com>
2023-12-07 17:27:55 +0900

    Create routes to display a form and the text based on the user's input

--- Day_57/server.py
+++ Day_57/server.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 from datetime import date
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    date_time = date.today()
-    year = date_time.year
-    random_number = random.randint(1,10)
-    return render_template("index.html", num=random_number, d=date_time, y=year)
+#@app.route('/')
+#def home():
+#    date_time = date.today()
+#    year = date_time.year
+#    random_number = random.randint(1,10)
+#    return render_template("index.html", num=random_number, d=date_time, y=year)
 
-@app.route('/guess/<name>')
-def guess(name):
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/guess', methods=["GET","POST"])
+def guess():
+    name = request.form.get("name", "world")
     gender_url = f"https://api.genderize.io?name={name}"
     gender_response = requests.get(gender_url)
     gender_response.raise_for_status()
@@ -26,12 +32,12 @@ def guess(name):
     return render_template("guess.html", name=name, gender=gender, age=age)
 
 
-@app.route('/blog')
-def blog():
-    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-    response = requests.get(blog_url)
-    all_posts = response.json()
-    return render_template("blog.html", posts=all_posts)
+#@app.route('/blog')
+#def blog():
+#    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+#    response = requests.get(blog_url)
+#    all_posts = response.json()
+#    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
