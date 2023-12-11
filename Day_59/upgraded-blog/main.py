77400bedfae04c93dc5b52aa3d492f434df66d8a
Camille Onoda <info@camilleonoda.com>
2023-12-11 12:34:38 +0900

    Create for loops in the index route and get_post route

--- Day_59/upgraded-blog/main.py
+++ Day_59/upgraded-blog/main.py
@@ -9,7 +9,13 @@ all_posts = response.json()
 
 @app.route('/')
 def index():
-    return render_template("index.html", posts=all_posts)
+    for blog_post in all_posts:
+        id = blog_post['id']
+        title = blog_post['title']
+        subtitle = blog_post['subtitle']
+        date = blog_post['date']
+    return render_template("index.html", title=title, subtitle=subtitle, date=date,
+                           id=id)
 
 
 @app.route('/about')
@@ -24,7 +30,11 @@ def contact():
 
 @app.route('/post/<int:index>')
 def get_post(index):
-    return render_template('post.html', posts=all_posts, id=index)
+    requested_post = None
+    for blog_post in all_posts:
+        if blog_post['id'] == index:
+            requested_post = blog_post
+    return render_template('post.html', post=requested_post)
 
 
 if __name__ == "__main__":
