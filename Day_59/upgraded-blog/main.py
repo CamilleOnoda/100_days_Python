7b551679449c25b93973655f30553fa73bfccb44
Camille Onoda <info@camilleonoda.com>
2023-12-11 12:00:22 +0900

    Create routes to the about and contact sections and to the individual blog posts

--- Day_59/upgraded-blog/main.py
+++ Day_59/upgraded-blog/main.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/c4d87a9e2fc58ee5b3a7"
+response = requests.get(blog_url)
+all_posts = response.json()
+
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post/<int:index>')
+def get_post(index):
+    return render_template('post.html', posts=all_posts, id=index)
 
 
 if __name__ == "__main__":
