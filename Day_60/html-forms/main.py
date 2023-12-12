a681f46134f782575269d49c53cf4474bd8aefad
Camille Onoda <info@camilleonoda.com>
2023-12-12 12:13:28 +0900

    Add request methods to use the name and password received via the post method in index.html

--- Day_60/html-forms/main.py
+++ Day_60/html-forms/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def hello():
     if request.method == "POST":
-        return render_template("greet.html", name=request.form.get("name", "world"))
+        return render_template("greet.html", name=request.form.get("name", "world"), password=request.form.get("password"))
     elif request.method == "GET":
         return render_template("index.html")
 
