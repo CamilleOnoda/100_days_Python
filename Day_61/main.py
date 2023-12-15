b67b2b37e3817d4903927e03160c4619ef857426
Camille Onoda <info@camilleonoda.com>
2023-12-15 12:07:51 +0900

    Add credentials validation

--- Day_61/main.py
+++ Day_61/main.py
@@ -27,7 +27,10 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/')
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html', form=form)
     return render_template('login.html', form=form)
 
 
