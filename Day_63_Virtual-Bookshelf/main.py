c31e3fede866e6032f2638955d25dbc19f7cafff
Camille Onoda <info@camilleonoda.com>
2023-12-20 18:01:56 +0900

    Create a list of dictionaries storing each book submitted with the form and display the book's info on the home page

--- Day_63_Virtual-Bookshelf/main.py
+++ Day_63_Virtual-Bookshelf/main.py
@@ -8,17 +8,20 @@ all_books = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', all_books=all_books)
 
 
 @app.route("/add", methods=['GET','POST'])
 def add():
     if request.method == 'POST':
-        return render_template('index.html', title=request.form.get("title"), 
-                               author=request.form.get("author"), 
-                               rating=request.form.get("rating"))
-    elif request.method == 'GET':
-        return render_template('add.html')
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book) 
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
