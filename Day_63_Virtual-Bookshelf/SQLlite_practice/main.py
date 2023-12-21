90413253a70776fa57eb4cee7429abf498e45bed
Camille Onoda <info@camilleonoda.com>
2023-12-21 12:25:31 +0900

    Delete a particular entry

--- Day_63_Virtual-Bookshelf/SQLlite_practice/main.py
+++ Day_63_Virtual-Bookshelf/SQLlite_practice/main.py
@@ -28,10 +28,10 @@ with app.app_context():
     db.create_all()
 
 # Add object to the session
-#with app.app_context():
-#    new_book = Book(id=2,title='Courage to be disliked',author='Koga Fumitake and Ichiro Kishimi',rating=9)
-#    db.session.add(new_book)
-#    db.session.commit()
+with app.app_context():
+    new_book = Book(id=2,title='Courage to be disliked',author='Koga Fumitake and Ichiro Kishimi',rating=9)
+    db.session.add(new_book)
+    db.session.commit()
 
 # Read all records
 with app.app_context():
@@ -44,11 +44,11 @@ with app.app_context():
     print(book)
 
 # Update a particular record by query
-#with app.app_context():
-#    book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter and the Chamber of Secrets")).scalar()
-#    book_to_update.title = "Harry Potter"
-#    book_to_update.rating = 3
-#    db.session.commit()
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter and the Chamber of Secrets")).scalar()
+    book_to_update.title = "Harry Potter"
+    book_to_update.rating = 3
+    db.session.commit()
 
 
 # Update a record by primary key
@@ -57,7 +57,21 @@ with app.app_context():
     book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
     book_to_update.title = "Harry Potter and the Goblet of Fire"
     db.session.commit()
+    
 
+# get_or_404() query method // returns None as the row with the given id doesn't exist
+book_id = 3
+with app.app_context():
+    book = db.get_or_404(Book, book_id)
+    
+
+# Delete a particular record by primary key
+book_id = 1
+with app.app_context():
+    #book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
 
 
 
