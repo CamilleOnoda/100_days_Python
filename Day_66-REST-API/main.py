f85a0ae13e54126b35fb57c4ebde680498efd96b
Camille Onoda <info@camilleonoda.com>
2024-01-07 06:43:04 +0900

    Create route to send a JSON response to the browser

---
+++ Day_66-REST-API/main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+
+app = Flask(__name__)
+
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# HTTP GET - Read Record
+@app.route("/random", methods=['GET'])
+def get_random_cafe():
+    all_cafes = list(db.session.execute(db.select(Cafe)).scalars())
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe={"id":random_cafe.id,
+                         "name":random_cafe.name,
+                         "map_url":random_cafe.map_url,
+                         "img_url":random_cafe.img_url,
+                         "location":random_cafe.location,
+                         "seats":random_cafe.seats,
+                         "has_toilet":random_cafe.has_toilet,
+                         "has_wifi":random_cafe.has_wifi,
+                         "has_sockets":random_cafe.has_sockets,
+                         "can_take_calls":random_cafe.can_take_calls,
+                         "coffee_price":random_cafe.coffee_price,
+                         })
+
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
